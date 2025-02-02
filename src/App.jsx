4053d06371c8cbd7b75bd52c11b7bb2364294e0a
Shatakshi <sshatakshi27@gmail.com>
2025-02-02 14:45:33 +0530

    Dashboard added

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-// src/App.jsx
-import { useUser } from '@clerk/clerk-react'
-import { Navigate} from 'react-router-dom'
+
+import { useUser } from '@clerk/clerk-react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Header from './components/Header';
 import SignInPage from './pages/SignInPage';
+import Dashboard from './pages/Dashboard';
 
 const App = () => {
-  const {user,isSignedIn}=useUser();
-
-  if(!isSignedIn)
-    {
-    return <Navigate to={'signin'}/>
-  }
-  
+  const { isSignedIn } = useUser();
 
   return (
- <>
- <Header/>
- <SignInPage />
- </>     
-  )
-}
+    <>
+      <Header />
+      <Routes>
+        {/* Redirect to SignInPage if not signed in */}
+        <Route path="/" element={isSignedIn ? <Navigate to="/dashboard" /> : <Navigate to="/signin" />} />
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/dashboard" element={isSignedIn ? <Dashboard /> : <Navigate to="/signin" />} />
+      </Routes>
+    </>
+  );
+};
 
 export default App;
